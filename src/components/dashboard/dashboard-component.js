d0a03871de6c358031e5670312dc09e9ced5378b
Hyesun An <anhyesun@gmail.com>
2021-06-17 08:04:24 -0700

    polish search bar

--- src/components/dashboard/dashboard-component.js
+++ src/components/dashboard/dashboard-component.js
@@ -1,20 +1,27 @@
 import './dashboard-component.css';
-import Input from '@material-ui/core/Input'
-import Button from '@material-ui/core/Button'
+
 import React, { useEffect, useState } from 'react';
 
+// import AccountCircle from '@material-ui/icons';
+import { Button } from 'semantic-ui-react';
 import CustomerInfoComponent from '../customer-info/info-component';
 import ListComponent from '../list/list-component';
+import TextField  from '@material-ui/core/TextField'
 import logo from '../../assets/citi.png'
 
 const DashBoardComponent = () => {
     return(
         <div>
-            <div className="header"><img src={logo}/></div>
-            <div classname="testing">
-                <p />Search Employee id              
-                <Input />
-                <Button variant="contained" color="primary">search</Button>
+            <div className="header">
+                <img src={logo}/> 
+                {/* <AccountCircle style={{alignSelf: "flex-end"}}/> */}
+            </div>
+            <div classname="search" 
+                style={{ display: "flex", flexDirection: "row", alignSelf: "conter", justifyContent: "center" }}>
+                <form className="input" noValidate autoComplete="off">
+                    <TextField id="standard-basic" label="Employee Id" />
+                </form>
+                <Button variant="contained" color="primary" onClick={alert}> Search </Button>  
             </div>
             <div>
                 <CustomerInfoComponent></CustomerInfoComponent>
